30f8224fabef2e1d4b1509addc2951cdae1a7bf9
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-05-13 14:03:00 +0200

    changed js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,32 +3,34 @@ import Notiflix from "notiflix";
 const formEl = document.getElementById("form");
 
 formEl.addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  const formData = new FormData(ev.target);
-  const firstDelay = parseInt(formData.get("delay"), 10);
-  const step = parseInt(formData.get("step"), 10);
-  const amount = parseInt(formData.get("amount"), 10);
+	ev.preventDefault();
 
-  for (let i = 0; i < amount; i++) {
-    createPromise(i + 1, firstDelay + i * step)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      });
-  }
+	const formData = new FormData(ev.target);
+	const delayEl = parseInt(formData.get("delay"), 10);
+	const stepEl = parseInt(formData.get("step"), 10);
+	const amountEl = parseInt(formData.get("amount"), 10);
+
+	for (let i = 0; i < amountEl; i++) {
+		createPromise(i + 1, delayEl + i * stepEl)
+			.then(({ position, delay }) => {
+				Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+			})
+			.catch(({ position, delay }) => {
+				Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+			});
+	}
 });
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const shouldResolve = Math.random() > 0.3;
+
+			if (shouldResolve) {
+				resolve({ position, delay });
+			} else {
+				reject({ position, delay });
+			}
+		}, delay);
+	});
 }
